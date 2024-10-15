e360f8aa86afbc205b565c8e05703210a7829f3d
LAPTOP-S37N1HI5\yeison <yeisoncalderonpoveda08@gmail.com>
2024-10-15 14:21:11 -0500

    IntroActivity

---
+++ app/src/main/java/com/sonyei888/onlineshopshoes/activity/BaseActivity.kt
@@ -0,0 +1,16 @@
+package com.sonyei888.onlineshopshoes.activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.sonyei888.onlineshopshoes.R
+
+class BaseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_base)
+
+    }
+}
--- app/src/main/java/com/sonyei888/onlineshopshoes/activity/IntroActivity.kt
+++ app/src/main/java/com/sonyei888/onlineshopshoes/activity/IntroActivity.kt
@@ -1,16 +1,25 @@
 package com.sonyei888.onlineshopshoes.activity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.sonyei888.onlineshopshoes.MainActivity
 import com.sonyei888.onlineshopshoes.R
+import com.sonyei888.onlineshopshoes.databinding.ActivityIntroBinding
 
 class IntroActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityIntroBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_intro)
+        binding = ActivityIntroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        binding.startBtn.setOnClickListener {
+            startActivity(Intent(this@IntroActivity, MainActivity::class.java))
+        }
     }
 }
