081a67136bc5023a8eb562d8f9b3893abd55d147
LAPTOP-S37N1HI5\yeison <yeisoncalderonpoveda08@gmail.com>
2024-10-15 14:09:18 -0500

    Diseño IntroActivity

---
+++ app/src/main/java/com/sonyei888/onlineshopshoes/activity/IntroActivity.kt
@@ -0,0 +1,16 @@
+package com.sonyei888.onlineshopshoes.activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.sonyei888.onlineshopshoes.R
+
+class IntroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+
+    }
+}
