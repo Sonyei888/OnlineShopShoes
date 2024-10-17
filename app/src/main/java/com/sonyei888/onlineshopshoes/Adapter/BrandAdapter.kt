e51d5cc9d113d6a34fa08938509fb191abfe70c0
LAPTOP-S37N1HI5\yeison <yeisoncalderonpoveda08@gmail.com>
2024-10-17 16:03:40 -0500

    BrandAdapter

---
+++ app/src/main/java/com/sonyei888/onlineshopshoes/Adapter/BrandAdapter.kt
@@ -0,0 +1,62 @@
+package com.sonyei888.onlineshopshoes.Adapter
+
+import android.content.Context
+import android.content.res.ColorStateList
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.widget.ImageViewCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.sonyei888.onlineshopshoes.R
+import com.sonyei888.onlineshopshoes.databinding.ActivityMainBinding
+import com.sonyei888.onlineshopshoes.databinding.ViewholderBrandBinding
+import com.sonyei888.onlineshopshoes.model.BrandModel
+
+class BrandAdapter(val items: MutableList<BrandModel>) :
+    RecyclerView.Adapter<BrandAdapter.Viewholder>() {
+
+    private var selectedPosition = -1
+    private var lastSelectedPosition = -1
+    private lateinit var context: Context
+
+    class Viewholder(val binding: ViewholderBrandBinding) : RecyclerView.ViewHolder(binding.root) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Viewholder {
+        context = parent.context
+        val binding = ViewholderBrandBinding.inflate(LayoutInflater.from(context), parent, false)
+        return Viewholder(binding)
+    }
+
+    override fun onBindViewHolder(holder: Viewholder, position: Int) {
+        val item = items[position]
+        holder.binding.title.text = item.title
+
+        Glide.with(holder.itemView.context)
+            .load(item.picUrl)
+            .into(holder.binding.pic)
+
+        holder.binding.root.setOnClickListener {
+            lastSelectedPosition = selectedPosition
+            selectedPosition = position
+            notifyItemChanged(lastSelectedPosition)
+            notifyItemChanged(selectedPosition)
+        }
+
+        holder.binding.title.setTextColor(context.resources.getColor(R.color.white))
+        if (selectedPosition == position) {
+            holder.binding.pic.setBackgroundColor(0)
+            holder.binding.mailLayout.setBackgroundResource(0)
+            ImageViewCompat.setImageTintList(
+                holder.binding.pic,
+                ColorStateList.valueOf(context.getColor(R.color.black))
+            )
+            holder.binding.title.visibility = View.GONE
+        }
+    }
+
+    override fun getItemCount(): Int = items.size
+
+}
