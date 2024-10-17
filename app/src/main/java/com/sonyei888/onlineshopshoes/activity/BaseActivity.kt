5b180846ed11b769f7be01f2d1e74451ebb2367b
LAPTOP-S37N1HI5\yeison <yeisoncalderonpoveda08@gmail.com>
2024-10-17 15:41:49 -0500

    Se desactivo el modo oscuro

--- app/src/main/java/com/sonyei888/onlineshopshoes/activity/BaseActivity.kt
+++ app/src/main/java/com/sonyei888/onlineshopshoes/activity/BaseActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.WindowManager
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.sonyei888.onlineshopshoes.R
@@ -11,6 +12,8 @@ import com.sonyei888.onlineshopshoes.R
 open class BaseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
 
         window.setFlags(
             WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
