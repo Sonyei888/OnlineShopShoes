76aff54c0a95de16efc91666ba0bfbc6bd081ed2
LAPTOP-S37N1HI5\yeison <yeisoncalderonpoveda08@gmail.com>
2024-10-17 15:04:13 -0500

    Se cambio de package MainActivity

--- app/src/main/java/com/sonyei888/onlineshopshoes/activity/IntroActivity.kt
+++ app/src/main/java/com/sonyei888/onlineshopshoes/activity/IntroActivity.kt
@@ -2,12 +2,6 @@ package com.sonyei888.onlineshopshoes.activity
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.sonyei888.onlineshopshoes.MainActivity
-import com.sonyei888.onlineshopshoes.R
 import com.sonyei888.onlineshopshoes.databinding.ActivityIntroBinding
 
 class IntroActivity : BaseActivity() {
--- app/src/main/java/com/sonyei888/onlineshopshoes/activity/MainActivity.kt
+++ app/src/main/java/com/sonyei888/onlineshopshoes/activity/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.sonyei888.onlineshopshoes
+package com.sonyei888.onlineshopshoes.activity
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.sonyei888.onlineshopshoes.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
