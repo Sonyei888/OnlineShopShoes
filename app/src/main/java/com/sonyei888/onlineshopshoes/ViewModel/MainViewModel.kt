c1488ae1af2951e448cbf38bd2fdc78b8adec344
LAPTOP-S37N1HI5\yeison <yeisoncalderonpoveda08@gmail.com>
2024-10-17 16:11:49 -0500

    MainViewModel

--- app/src/main/java/com/sonyei888/onlineshopshoes/ViewModel/MainViewModel.kt
+++ app/src/main/java/com/sonyei888/onlineshopshoes/ViewModel/MainViewModel.kt
@@ -7,12 +7,18 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.sonyei888.onlineshopshoes.model.BrandModel
 import com.sonyei888.onlineshopshoes.model.SliderModel
+import java.sql.Ref
 
 class MainViewModel():ViewModel() {
     private val firebaseDatabase= FirebaseDatabase.getInstance()
+
     private val _banner=MutableLiveData<List<SliderModel>>()
+    private val _brand = MutableLiveData<MutableList<BrandModel>>()
+
     val banners: LiveData<List<SliderModel>> = _banner
+    val brands: LiveData<MutableList<BrandModel>> = _brand
 
     fun loadBanners(){
         val Ref=firebaseDatabase.getReference("Banner")
@@ -29,6 +35,25 @@ class MainViewModel():ViewModel() {
             }
 
             override fun onCancelled(error: DatabaseError) {
+            }
+        })
+    }
+    fun loadBrand(){
+        val Ref = firebaseDatabase.getReference("Category")
+        Ref.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val lists = mutableListOf<BrandModel>()
+                for (childSnapshot in snapshot.children){
+                    val list = childSnapshot.getValue(BrandModel::class.java)
+                    if (list != null){
+                        lists.add(list)
+                    }
+                }
+                _brand.value=lists
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
             }
         })
     }
