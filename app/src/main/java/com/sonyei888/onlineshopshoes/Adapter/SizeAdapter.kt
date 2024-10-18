5ed09ae2e4f9f02ee0b988ff00918df38be2841b
LAPTOP-S37N1HI5\yeison <yeisoncalderonpoveda08@gmail.com>
2024-10-18 15:18:22 -0500

    SizeAdapter

---
+++ app/src/main/java/com/sonyei888/onlineshopshoes/Adapter/SizeAdapter.kt
@@ -0,0 +1,59 @@
+package com.sonyei888.onlineshopshoes.Adapter
+
+import android.content.Context
+import android.content.res.ColorStateList
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.widget.ImageViewCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.sonyei888.onlineshopshoes.R
+import com.sonyei888.onlineshopshoes.databinding.ActivityMainBinding
+import com.sonyei888.onlineshopshoes.databinding.ViewholderBrandBinding
+import com.sonyei888.onlineshopshoes.databinding.ViewholderColorBinding
+import com.sonyei888.onlineshopshoes.databinding.ViewholderSizeBinding
+import com.sonyei888.onlineshopshoes.model.BrandModel
+
+class SizeAdapter(val items: MutableList<String>) :
+    RecyclerView.Adapter<SizeAdapter.Viewholder>() {
+
+    private var selectedPosition = -1
+    private var lastSelectedPosition = -1
+    private lateinit var context: Context
+
+    class Viewholder(val binding: ViewholderSizeBinding) : RecyclerView.ViewHolder(binding.root) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SizeAdapter.Viewholder {
+        context = parent.context
+        val binding = ViewholderSizeBinding.inflate(LayoutInflater.from(context), parent, false)
+        return Viewholder(binding)
+    }
+
+    override fun onBindViewHolder(holder: Viewholder, position: Int) {
+
+        holder.binding.sizeTxt.text=items[position]
+
+        holder.binding.root.setOnClickListener {
+            lastSelectedPosition = selectedPosition
+            selectedPosition = position
+            notifyItemChanged(lastSelectedPosition)
+            notifyItemChanged(selectedPosition)
+        }
+
+
+        if (selectedPosition == position) {
+
+            holder.binding.colorLayout.setBackgroundResource(R.drawable.grey_bg_selected)
+            holder.binding.sizeTxt.setTextColor(context.resources.getColor(R.color.purple))
+        } else {
+            holder.binding.colorLayout.setBackgroundResource(R.drawable.grey_bg)
+            holder.binding.sizeTxt.setTextColor(context.resources.getColor(R.color.black))
+        }
+    }
+
+    override fun getItemCount(): Int = items.size
+
+}
