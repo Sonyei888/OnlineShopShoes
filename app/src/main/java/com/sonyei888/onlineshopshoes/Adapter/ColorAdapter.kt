5b73bef33d49ce54caa895f28f043031a3cd5a97
LAPTOP-S37N1HI5\yeison <yeisoncalderonpoveda08@gmail.com>
2024-10-18 15:07:43 -0500

    viewholder_size

--- app/src/main/java/com/sonyei888/onlineshopshoes/Adapter/ColorAdapter.kt
+++ app/src/main/java/com/sonyei888/onlineshopshoes/Adapter/ColorAdapter.kt
@@ -11,6 +11,7 @@ import com.bumptech.glide.Glide
 import com.sonyei888.onlineshopshoes.R
 import com.sonyei888.onlineshopshoes.databinding.ActivityMainBinding
 import com.sonyei888.onlineshopshoes.databinding.ViewholderBrandBinding
+import com.sonyei888.onlineshopshoes.databinding.ViewholderColorBinding
 import com.sonyei888.onlineshopshoes.model.BrandModel
 
 class ColorAdapter(val items: MutableList<String>) :
@@ -20,23 +21,22 @@ class ColorAdapter(val items: MutableList<String>) :
     private var lastSelectedPosition = -1
     private lateinit var context: Context
 
-    class Viewholder(val binding: ViewholderBrandBinding) :
+    class Viewholder(val binding: ViewholderColorBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ColorAdapter.Viewholder {
         context = parent.context
-        val binding = ViewholderBrandBinding.inflate(LayoutInflater.from(context), parent, false)
+        val binding = ViewholderColorBinding.inflate(LayoutInflater.from(context), parent, false)
         return Viewholder(binding)
     }
 
     override fun onBindViewHolder(holder: Viewholder, position: Int) {
         val item = items[position]
-        holder.binding.title.text = item.title
 
         Glide.with(holder.itemView.context)
-            .load(item.picUrl)
+            .load(items[position])
             .into(holder.binding.pic)
 
         holder.binding.root.setOnClickListener {
@@ -46,23 +46,14 @@ class ColorAdapter(val items: MutableList<String>) :
             notifyItemChanged(selectedPosition)
         }
 
-        holder.binding.title.setTextColor(context.resources.getColor(R.color.white))
+
         if (selectedPosition == position) {
-            holder.binding.pic.setBackgroundResource(0)
-            holder.binding.mailLayout.setBackgroundResource(R.drawable.puple_bg)
-            ImageViewCompat.setImageTintList(
-                holder.binding.pic,
-                ColorStateList.valueOf(context.getColor(R.color.white))
-            )
-            holder.binding.title.visibility = View.VISIBLE
+
+            holder.binding.colorLayout.setBackgroundResource(R.drawable.puple_bg)
+
         } else {
-            holder.binding.pic.setBackgroundResource(R.drawable.grey_bg)
-            holder.binding.mailLayout.setBackgroundResource(0)
-            ImageViewCompat.setImageTintList(
-                holder.binding.pic,
-                ColorStateList.valueOf(context.getColor(R.color.black))
-            )
-            holder.binding.title.visibility = View.GONE
+            holder.binding.colorLayout.setBackgroundResource(R.drawable.grey_bg)
+
         }
     }
 
